4f40a28efd7e2e6de5d14c2d4c5ccea4061f3008
Matthew Draper <matt.draper@lynch2.com>
2019-11-27 15:20:57 -0600

    Describe connection

--- routes/web.php
+++ routes/web.php
@@ -22,20 +22,56 @@ Route::get('/callback', function (Request $request) {
             'client_secret' => getenv('OAUTH_CLIENT_SECRET'), // Replace with client secret
             'redirect_uri' => 'http://oauth-client.test/callback',
             'code' => $request->code,
-        ]
+        ],
     ]);
-
     session()->put('token', json_decode((string) $response->getBody(), true));
 
-    return redirect('/user');
+    return redirect('/resource');
 });
 
-Route::get('/user', function () {
-    $response = (new GuzzleHttp\Client)->get('https://oauth.test/api/user', [
+Route::get('/resource/{id}', function ($id) {
+    $url = 'https://oauth.test/api/resource/'.$id;
+    $response = (new GuzzleHttp\Client)->get($url, [
+        'headers' => [
+            'Authorization' => 'Bearer '.session()->get('token.access_token')
+        ],
+        'verify' => false,
+    ]);
+    $json = json_decode((string) $response->getBody(), true);
+    return json_decode(json_encode($json, JSON_PRETTY_PRINT), true);
+});
+
+Route::get('/resource', function () {
+    $response = (new GuzzleHttp\Client)->get('https://oauth.test/api/resource', [
+        'headers' => [
+            'Authorization' => 'Bearer '.session()->get('token.access_token')
+        ],
+        'verify' => false,
+    ]);
+    $json = json_decode((string) $response->getBody(), true);
+    return json_decode(json_encode($json, JSON_PRETTY_PRINT), true);
+});
+
+
+
+Route::get('/contact', function () {
+    $response = (new GuzzleHttp\Client)->get('https://oauth.test/api/contact', [
         'headers' => [
             'Authorization' => 'Bearer '.session()->get('token.access_token')
         ],
         'verify' => false,
     ]);
     return json_decode((string) $response->getBody(), true);
+//    return json_decode(json_encode($json, JSON_PRETTY_PRINT), true);
+});
+
+Route::get('/describe-connection', function () {
+    $response = (new GuzzleHttp\Client)->get('https://oauth.test/api/describe-connection', [
+        'headers' => [
+            'Authorization' => 'Bearer '.session()->get('token.access_token')
+        ],
+        'verify' => false,
+    ]);
+    return json_decode((string) $response->getBody(), true);
+//    return json_decode(json_encode($json, JSON_PRETTY_PRINT), true);
 });
